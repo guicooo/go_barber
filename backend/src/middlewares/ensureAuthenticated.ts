00e9134df007abbb6297cb4d2e55b5f04f24aebb
Guilherme Cordeiro <guico_@hotmail.com>
2020-11-16 11:17:40 -0300

    authorization header / user id header

--- backend/src/middlewares/ensureAuthenticated.ts
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,13 @@
+import { sub } from "date-fns";
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import auth from "../config/auth";
+
+interface TokenPayload {
+  iat: number,
+  exp: number,
+  sub: string,
+}
 
 export default function ensureAuthenticated(request: Request, response: Response, next: NextFunction): void {
 
@@ -16,10 +23,13 @@ export default function ensureAuthenticated(request: Request, response: Response
   try {
     const decoded = verify(token, auth.jwt.subject);
 
-    console.log(decoded);
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub
+    }
 
     return next();
-
   } catch {
     throw new Error('Invalid  JWT token')
   }
