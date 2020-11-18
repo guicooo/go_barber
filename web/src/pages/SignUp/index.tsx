7a3ece76d9fde2f9488d2da04492ea676e90361f
Guilherme Cordeiro <guico_@hotmail.com>
2020-11-18 11:33:51 -0300

    forms

---
+++ web/src/pages/SignUp/index.tsx
@@ -0,0 +1,57 @@
+import React, { useCallback, useRef } from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+
+import logo from '../../assets/logo.svg';
+
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+function handleSubmit (data: Object) {
+    console.log(data);
+    
+}
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+          <img src={logo} alt="GoBarber" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
+
+            <Input name="name" placeholder="Nome" icon={FiUser} />
+            <Input name="email" placeholder="E-mail" icon={FiMail} />
+            <Input
+              name="password"
+              type="password"
+              placeholder="Senha"
+              icon={FiLock}
+            />
+            <Button type="submit">Cadastrar</Button>
+          </Form>
+
+            <FiArrowLeft />
+            Voltar para logon
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
